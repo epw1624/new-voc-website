815aaff06d284a042917d48c13115e43a7ee10ca
epw1624 <emily.wood1624@gmail.com>
2026-03-13 15:54:54 -0700

    Increase max length on image field

--- ubc_voc_website/gallery/models.py
+++ ubc_voc_website/gallery/models.py
@@ -5,7 +5,7 @@ class GalleryPhoto(models.Model):
     title = models.CharField(max_length=255, blank=True)
     description = models.TextField(max_length=1024, blank=True, null=True)
     album = models.CharField(max_length=255, db_index=True, blank=True, null=True)
-    image = models.ImageField(storage=LegacyGalleryStorage())
+    image = models.ImageField(storage=LegacyGalleryStorage(), max_length=1024)
 
     def __str__(self):
         return self.title or f"Photo: {self.id}"
