f7aefcced30ba8627cc3cc0faee8b854131905cb
epw1624 <emily.wood1624@gmail.com>
2026-03-28 19:12:12 -0700

    Support comment replies when importing legacy trip report comments

--- ubc_voc_website/tripreports/management/commands/migrate_trip_report_comments.py
+++ ubc_voc_website/tripreports/management/commands/migrate_trip_report_comments.py
@@ -1,5 +1,5 @@
 """
-
+SELECT comment_ID, comment_post_ID, comment_author_email, comment_date, comment_parent, comment_content FROM wp_comments
 """
 from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
@@ -20,46 +20,65 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         path = "trip_report_comments.csv"
 
+        id_map = {}
+        parent_map = {}
+
+        orphaned_emails = set()
+        missing_trip_reports = set()
+
         with open(path, newline="", encoding="utf-8") as f:
             reader = csv.DictReader(f, fieldnames=[
+                "comment_ID",
                 "comment_post_ID",
                 "comment_author_email",
                 "comment_date",
+                "comment_parent",
                 "comment_content"
             ])
 
-            orphaned_emails = set()
-            missing_trip_reports = set()
+            self.stdout.write("Step 1: Finding all comment parents...")
+            rows = list(reader)
+            for row in rows:
+                old_id = row["comment_ID"]
+                old_parent = row["comment_parent"]
+                parent_map[old_id] = old_parent
 
-            for row in reader:
                 try:
                     user = User.objects.get(email=row["comment_author_email"])
+                    trip_report = TripReport.objects.get(old_id=int(row["comment_post_ID"]))
+                    
+                    dt = datetime.strptime(row["comment_date"], "%Y-%m-%d %H:%M:%S")
+                    dt = dt.replace(tzinfo=pacific_timezone)
+
+                    comment, created = Comment.objects.get_or_create(
+                        timestamp=dt,
+                        user=user,
+                        trip_report=trip_report,
+                        defaults={"body": row["comment_content"]}
+                    )
+                    
+                    id_map[old_id] = comment
                 except User.DoesNotExist:
                     orphaned_emails.add(row["comment_author_email"])
-                    self.stdout.write(self.style.WARNING(f"User not found with email {row["comment_author_email"]}"))
                     continue
-
-                try:
-                    trip_report = TripReport.objects.get(old_id=int(row["comment_post_ID"]))
                 except TripReport.DoesNotExist:
-                    missing_trip_reports.add(int(row["comment_post_ID"]))
-                    self.stdout.write(self.style.WARNING(f"Trip report with old_id {int(row["comment_post_ID"])} does not exist"))
+                    missing_trip_reports.add(row["comment_post_ID"])
                     continue
 
-                comment, created = Comment.objects.get_or_create(
-                    trip_report=trip_report,
-                    user=user,
-                    timestamp=datetime.strptime(row["comment_date"], "%Y-%m-%d %H:%M:%S").replace(tzinfo=pacific_timezone),
-                    defaults={
-                        "body": row["comment_content"]
-                    }
-                )
-
-                if created:
-                    comment.save()
-                    self.stdout.write(self.style.SUCCESS(f"Created comment from {user.display_name} on trip report with old_id {int(row["comment_post_ID"])}"))
+            self.stdout.write("Step 2: Re-mapping comment parents")
+            for old_id, comment in id_map.items():
+                curr_parent_id = parent_map.get(old_id)
+                if curr_parent_id == "0":
+                    continue
                 else:
-                    self.stdout.write(f"Comment from {user.display_name} on post with old_id {int(row["comment_post_ID"])} already exists")
+                    root_parent_id = curr_parent_id
+                    while parent_map.get(root_parent_id) != "0":
+                        root_parent_id = parent_map[root_parent_id]
+
+                    new_parent_id = id_map.get(root_parent_id)
+                    if new_parent_id and new_parent_id != comment:
+                        comment.parent = new_parent_id
+                        comment.save()
             
             self.stdout.write(self.style.SUCCESS("Trip report comment migration complete"))
             self.stdout.write(self.style.WARNING(f"Orphaned emails: {orphaned_emails}"))
