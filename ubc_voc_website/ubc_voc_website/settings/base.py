a4952fa2b539082fdccd68b81b6718723f752389
epw1624 <emily.wood1624@gmail.com>
2026-03-30 13:06:58 -0700

    Fix persistent volume for message board index

--- ubc_voc_website/ubc_voc_website/settings/base.py
+++ ubc_voc_website/ubc_voc_website/settings/base.py
@@ -193,7 +193,7 @@ CACHES = {
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-        'PATH': '/home/ubc_voc_website/whoosh_index'
+        'PATH': '/home/ubc_voc_website/whoosh_data'
     },
 }
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
